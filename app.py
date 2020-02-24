47c9d0ce2ebe8f14328e200972e6962e5d0fb5d4
anir <amukherjee0411@gmail.com>
2020-02-23 21:07:34 -0500

    final code

--- app.py
+++ app.py
@@ -1,29 +1,40 @@
-# import necessary libraries
-from flask import Flask, render_template
-import pymongo
-import scrape
+#################################################
+# MongoDB and Flask Application
+#################################################
 
-# create instance of Flask app
+# Dependencies and Setup
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scrape_mars
+
+#################################################
+# Flask Setup
+#################################################
 app = Flask(__name__)
 
-# create mongo connection
-conn = 'mongodb://localhost:27017'
-client = pymongo.MongoClient(conn)
-db = client.mars_db
-collection = db.mars_data_entries
+#################################################
+# PyMongo Connection Setup
+#################################################
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
 
+#################################################
+# Flask Routes
+#################################################
+# Root Route to Query MongoDB & Pass Mars Data Into HTML Template: index.html to Display Data
 @app.route("/")
-def home():
-    mars_data = list(db.mars_data_entries.find())[0]
-    print(mars_data)
-    return  render_template('index.html', mars_data=mars_data)
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
 
+# Scrape Route to Import `scrape_mars.py` Script & Call `scrape` Function
 @app.route("/scrape")
-def web_scrape():
-    db.collection.remove({})
-    mars_data = scrape.scrape()
-    db.collection.insert_one(mars_data)
-    return  render_template('scrape.html')
+def scrapper():
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape_all()
+    mars.update({}, mars_data, upsert=True)
+    return "Scraping Successful"
 
+# Define Main Behavior
 if __name__ == "__main__":
     app.run()
